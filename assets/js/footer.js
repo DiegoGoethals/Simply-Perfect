009f33e3eec44172064409a1adc318c3a8405cd8
diego <goethalsdiego@gmail.com>
2022-11-08 16:34:47 +0100

    Made first part of footer

---
+++ assets/js/footer.js
@@ -0,0 +1,11 @@
+"use strict";
+
+init();
+
+function init() {
+    const contactInfo = `<div id="contactInfo"><h1>CONTACT INFO</h1><p>&nbsp;</p><span>📍 Generaal Lemanlaan 100 / 8310 Assebroek</span>
+                           <span>🕚 MA-ZA 9u30 - 22u</span>
+                           <a href="mailto:info@simplyperfectaccesoires.be">✉️ info@simplyperfectaccesoires.be</a><p>&nbsp;</p>
+                           </div>`;
+    document.querySelector("footer").insertAdjacentHTML("afterbegin", contactInfo);
+}
