ba066b5606ede3fd22c224e5832470bada7c9b08
diego <goethalsdiego@gmail.com>
2022-11-18 23:40:01 +0100

    Added fetch script

---
+++ assets/js/config.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const _config = {
+    URL: "http://localhost:8080"
+};
--- assets/js/shop.js
+++ assets/js/shop.js
@@ -4,4 +4,11 @@ init();
 
 function init() {
     document.querySelector("title").innerHTML = localStorage.getItem("active");
+    fillShop();
+}
+
+function fillShop() {
+    fetchFromServer(`${_config.URL}/products`, "GET").then(result => {
+        console.log(result);
+    });
 }
